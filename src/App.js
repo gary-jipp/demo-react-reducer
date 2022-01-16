9b0620a61c8082b492b2073689323f13ccb9b0a3
Gary J <github@accu.ca>
2022-01-15 20:36:39 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import { useState } from "react";
+import 'App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = function() {
+    setCount(count + 1);
+  };
+  const decrement = function() {
+    setCount(count - 1);
+  };
+  const clear = function() {
+    setCount(0);
+  };
 
   return (
     <div className="App">
       <h1>
-        Hello React
+        Counters!
       </h1>
+
+      <div class="counter">
+        <span>{count}</span>
+        <button onClick={decrement}>-</button>
+        <button onClick={clear}>0</button>
+        <button onClick={increment}>+</button>
+      </div>
     </div>
   );
 }
